3832153c296b0afc7727df7023e59d0f9a6685b4
Hani <aboahmedhani82@gmail.com>
2024-06-14 23:47:00 +0300

    minor fix for constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 // TODO: make a new db
-export const invalidAnswers: string[] = ["yes", "I don't know", "no", "that's fine"]
+export const invalidAnswers: string[] = ["yes", "i don't know", "no", "that's fine"]
